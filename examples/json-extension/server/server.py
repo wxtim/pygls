d8cd0376b85177c2e8af225f587085e7efd68d95
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2023-01-31 11:06:24 +0000

    added auto-completions

--- examples/json-extension/server/server.py
+++ examples/json-extension/server/server.py
@@ -14,36 +14,36 @@
 # See the License for the specific language governing permissions and      #
 # limitations under the License.                                           #
 ############################################################################
-import asyncio
-import json
-import re
-import time
-import uuid
-from json import JSONDecodeError
+from pathlib import Path
 from typing import Optional
 
-from lsprotocol.types import (TEXT_DOCUMENT_COMPLETION, TEXT_DOCUMENT_DID_CHANGE,
-                               TEXT_DOCUMENT_DID_CLOSE, TEXT_DOCUMENT_DID_OPEN,
-                               TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL)
-from lsprotocol.types import (CompletionItem, CompletionList, CompletionOptions,
-                              CompletionParams, ConfigurationItem,
-                              Diagnostic,
-                              DidChangeTextDocumentParams,
-                              DidCloseTextDocumentParams,
-                              DidOpenTextDocumentParams, MessageType, Position,
-                              Range, Registration, RegistrationParams,
-                              SemanticTokens, SemanticTokensLegend, SemanticTokensParams,
-                              Unregistration, UnregistrationParams,
-                              WorkDoneProgressBegin, WorkDoneProgressEnd,
-                              WorkDoneProgressReport,
-                              WorkspaceConfigurationParams)
+from cylc.flow.cfgspec.workflow import SPEC
+from cylc.flow.scripts.lint import check_cylc_file, parse_checks
+from cylc.flow.scripts.validate import wrapped_main as cylc_validate, ValidateOptions
+
+from lsprotocol.types import (
+    TEXT_DOCUMENT_COMPLETION, TEXT_DOCUMENT_DID_CHANGE,
+    TEXT_DOCUMENT_DID_CLOSE, TEXT_DOCUMENT_DID_OPEN,
+    TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL
+)
+from lsprotocol.types import (
+    CompletionItem, CompletionList, CompletionOptions,
+    CompletionParams, ConfigurationItem,
+    Diagnostic,
+    DidChangeTextDocumentParams,
+    DidCloseTextDocumentParams,
+    DidOpenTextDocumentParams, MessageType, Position,
+    Range, Registration, RegistrationParams,
+    SemanticTokens, SemanticTokensLegend, SemanticTokensParams,
+    Unregistration, UnregistrationParams,
+    WorkDoneProgressBegin, WorkDoneProgressEnd,
+    WorkDoneProgressReport,
+    WorkspaceConfigurationParams
+)
 from pygls.server import LanguageServer
 
-COUNT_DOWN_START_IN_SECONDS = 10
-COUNT_DOWN_SLEEP_IN_SECONDS = 1
 
-
-class JsonLanguageServer(LanguageServer):
+class CylcLanguageServer(LanguageServer):
     CMD_COUNT_DOWN_BLOCKING = 'countDownBlocking'
     CMD_COUNT_DOWN_NON_BLOCKING = 'countDownNonBlocking'
     CMD_PROGRESS = 'progress'
@@ -59,163 +59,161 @@ class JsonLanguageServer(LanguageServer):
         super().__init__(*args)
 
 
-json_server = JsonLanguageServer('pygls-json-example', 'v0.1')
+cylc_ls = CylcLanguageServer('Cylc Language Server', 'v0.1')
 
 
-def _validate(ls, params):
-    ls.show_message_log('Validating json...')
+async def _validate(ls, params):
+    """Run Cylc lint & Cylc validate
+
+    n.b: Cylc Validate will only return on issue at a time.
+    """
+    ls.show_message_log('Validating cylc...')
 
     text_doc = ls.workspace.get_document(params.text_document.uri)
-
     source = text_doc.source
-    diagnostics = _validate_json(source) if source else []
+
+    # Run Cylc Lint:
+    checks = parse_checks(['style'])
+    diagnostics = check_cylc_file(
+        source, for_language_server=True, checks=checks
+    ) if source else []
+    diagnostics = [Diagnostic(**d) for d in diagnostics]
+
+    # Add Cylc Validate diagnostics:
+    diagnostics += await _validate_cylc(text_doc.uri)
 
     ls.publish_diagnostics(text_doc.uri, diagnostics)
 
 
-def _validate_json(source):
-    """Validates json file."""
+async def _validate_cylc(text_doc):
+    """Run `cylc validate`
+    """
+    docpath = str(Path(text_doc.split(':')[1]).parent)
+    options = ValidateOptions()
+
     diagnostics = []
-
     try:
-        json.loads(source)
-    except JSONDecodeError as err:
-        msg = err.msg
-        col = err.colno
-        line = err.lineno
-
+        await cylc_validate(options, docpath)
+    except Exception as exc:
         d = Diagnostic(
-            range=Range(
-                start=Position(line=line - 1, character=col - 1),
-                end=Position(line=line - 1, character=col)
-            ),
-            message=msg,
-            source=type(json_server).__name__
+            range=Range(Position(0, 0), Position(0, 10)),
+            message=str(exc),
+            source='cylc validate'
         )
-
         diagnostics.append(d)
-
     return diagnostics
 
 
-@json_server.feature(TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=[',']))
+@cylc_ls.feature(TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=[',']))
 def completions(params: Optional[CompletionParams] = None) -> CompletionList:
     """Returns completion items."""
+
+    # Extract a list of valid cylc settings from the config:
+    cylc_completions = list(set([
+        i.strip(' []')
+        for i in SPEC.tree().split('\n')
+        if '<' not in i and '>' not in i
+    ]))
+    cylc_completions = [
+        CompletionItem(label=i) for i in cylc_completions]
+
     return CompletionList(
         is_incomplete=False,
         items=[
             CompletionItem(label='"'),
             CompletionItem(label='['),
             CompletionItem(label=']'),
-            CompletionItem(label='{'),
-            CompletionItem(label='}'),
-        ]
+        ] +
+        cylc_completions[1:]   # not the "flow.cylc"
     )
 
 
-@json_server.command(JsonLanguageServer.CMD_COUNT_DOWN_BLOCKING)
-def count_down_10_seconds_blocking(ls, *args):
-    """Starts counting down and showing message synchronously.
-    It will `block` the main thread, which can be tested by trying to show
-    completion items.
-    """
-    for i in range(COUNT_DOWN_START_IN_SECONDS):
-        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
-        time.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
-
-
-@json_server.command(JsonLanguageServer.CMD_COUNT_DOWN_NON_BLOCKING)
-async def count_down_10_seconds_non_blocking(ls, *args):
-    """Starts counting down and showing message asynchronously.
-    It won't `block` the main thread, which can be tested by trying to show
-    completion items.
-    """
-    for i in range(COUNT_DOWN_START_IN_SECONDS):
-        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
-        await asyncio.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
-
-
-@json_server.feature(TEXT_DOCUMENT_DID_CHANGE)
-def did_change(ls, params: DidChangeTextDocumentParams):
+@cylc_ls.feature(TEXT_DOCUMENT_DID_CHANGE)
+# @cylc_ls.command(CylcLanguageServer.CMD_COUNT_DOWN_NON_BLOCKING)
+async def did_change(ls, params: DidChangeTextDocumentParams):
     """Text document did change notification."""
-    _validate(ls, params)
+    await _validate(ls, params)
 
 
-@json_server.feature(TEXT_DOCUMENT_DID_CLOSE)
-def did_close(server: JsonLanguageServer, params: DidCloseTextDocumentParams):
+@cylc_ls.feature(TEXT_DOCUMENT_DID_CLOSE)
+def did_close(server: CylcLanguageServer, params: DidCloseTextDocumentParams):
     """Text document did close notification."""
-    server.show_message('Text Document Did Close')
+    server.show_message('Closed Cylc Config')
 
 
-@json_server.feature(TEXT_DOCUMENT_DID_OPEN)
+@cylc_ls.feature(TEXT_DOCUMENT_DID_OPEN)
 async def did_open(ls, params: DidOpenTextDocumentParams):
     """Text document did open notification."""
-    ls.show_message('Text Document Did Open')
-    _validate(ls, params)
-
-
-@json_server.feature(
-    TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,
-    SemanticTokensLegend(
-        token_types = ["operator"],
-        token_modifiers = []
+    ls.show_message(
+        'Opened a Cylc Config\n'
+        'T[https://cylc.github.io/cylc-doc/stable/html/'
+        'reference/config/workflow.html]'
     )
-)
-def semantic_tokens(ls: JsonLanguageServer, params: SemanticTokensParams):
-    """See https://microsoft.github.io/language-server-protocol/specification#textDocument_semanticTokens
-    for details on how semantic tokens are encoded."""
-
-    TOKENS = re.compile('".*"(?=:)')
-
-    uri = params.text_document.uri
-    doc = ls.workspace.get_document(uri)
-
-    last_line = 0
-    last_start = 0
-
-    data = []
-
-    for lineno, line in enumerate(doc.lines):
-        last_start = 0
-
-        for match in TOKENS.finditer(line):
-            start, end = match.span()
-            data += [
-                (lineno - last_line),
-                (start - last_start),
-                (end - start),
-                0,
-                0
-            ]
-
-            last_line = lineno
-            last_start = start
-
-    return SemanticTokens(data=data)
+    await _validate(ls, params)
 
 
+# @cylc_ls.feature(
+#     TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,
+#     SemanticTokensLegend(
+#         token_types = ["operator"],
+#         token_modifiers = []
+#     )
+# )
+# def semantic_tokens(ls: CylcLanguageServer, params: SemanticTokensParams):
+#     """See https://microsoft.github.io/language-server-protocol/specification#textDocument_semanticTokens
+#     for details on how semantic tokens are encoded."""
 
-@json_server.command(JsonLanguageServer.CMD_PROGRESS)
-async def progress(ls: JsonLanguageServer, *args):
-    """Create and start the progress on the client."""
-    token = str(uuid.uuid4())
-    # Create
-    await ls.progress.create_async(token)
-    # Begin
-    ls.progress.begin(token, WorkDoneProgressBegin(title='Indexing', percentage=0))
-    # Report
-    for i in range(1, 10):
-        ls.progress.report(
-            token,
-            WorkDoneProgressReport(message=f'{i * 10}%', percentage= i * 10),
-        )
-        await asyncio.sleep(2)
-    # End
-    ls.progress.end(token, WorkDoneProgressEnd(message='Finished'))
+#     TOKENS = re.compile('".*"(?=:)')
+
+#     uri = params.text_document.uri
+#     doc = ls.workspace.get_document(uri)
+
+#     last_line = 0
+#     last_start = 0
+
+#     data = []
+
+#     for lineno, line in enumerate(doc.lines):
+#         last_start = 0
+
+#         for match in TOKENS.finditer(line):
+#             start, end = match.span()
+#             data += [
+#                 (lineno - last_line),
+#                 (start - last_start),
+#                 (end - start),
+#                 0,
+#                 0
+#             ]
+
+#             last_line = lineno
+#             last_start = start
+
+#     return SemanticTokens(data=data)
 
 
-@json_server.command(JsonLanguageServer.CMD_REGISTER_COMPLETIONS)
-async def register_completions(ls: JsonLanguageServer, *args):
+
+# @cylc_ls.command(CylcLanguageServer.CMD_PROGRESS)
+# async def progress(ls: CylcLanguageServer, *args):
+#     """Create and start the progress on the client."""
+#     token = str(uuid.uuid4())
+#     # Create
+#     await ls.progress.create_async(token)
+#     # Begin
+#     ls.progress.begin(token, WorkDoneProgressBegin(title='Indexing', percentage=0))
+#     # Report
+#     for i in range(1, 10):
+#         ls.progress.report(
+#             token,
+#             WorkDoneProgressReport(message=f'{i * 10}%', percentage= i * 10),
+#         )
+#         await asyncio.sleep(2)
+#     # End
+#     ls.progress.end(token, WorkDoneProgressEnd(message='Finished'))
+
+
+@cylc_ls.command(CylcLanguageServer.CMD_REGISTER_COMPLETIONS)
+async def register_completions(ls: CylcLanguageServer, *args):
     """Register completions method on the client."""
     params = RegistrationParams(registrations=[
                 Registration(
@@ -231,77 +229,77 @@ async def register_completions(ls: JsonLanguageServer, *args):
                         MessageType.Error)
 
 
-@json_server.command(JsonLanguageServer.CMD_SHOW_CONFIGURATION_ASYNC)
-async def show_configuration_async(ls: JsonLanguageServer, *args):
-    """Gets exampleConfiguration from the client settings using coroutines."""
-    try:
-        config = await ls.get_configuration_async(
-            WorkspaceConfigurationParams(items=[
-                ConfigurationItem(
-                    scope_uri='',
-                    section=JsonLanguageServer.CONFIGURATION_SECTION)
-        ]))
+# @cylc_ls.command(CylcLanguageServer.CMD_SHOW_CONFIGURATION_ASYNC)
+# async def show_configuration_async(ls: CylcLanguageServer, *args):
+#     """Gets exampleConfiguration from the client settings using coroutines."""
+#     try:
+#         config = await ls.get_configuration_async(
+#             WorkspaceConfigurationParams(items=[
+#                 ConfigurationItem(
+#                     scope_uri='',
+#                     section=CylcLanguageServer.CONFIGURATION_SECTION)
+#         ]))
 
-        example_config = config[0].get('exampleConfiguration')
+#         example_config = config[0].get('exampleConfiguration')
 
-        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+#         ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
 
-    except Exception as e:
-        ls.show_message_log(f'Error ocurred: {e}')
-
-
-@json_server.command(JsonLanguageServer.CMD_SHOW_CONFIGURATION_CALLBACK)
-def show_configuration_callback(ls: JsonLanguageServer, *args):
-    """Gets exampleConfiguration from the client settings using callback."""
-    def _config_callback(config):
-        try:
-            example_config = config[0].get('exampleConfiguration')
-
-            ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
-
-        except Exception as e:
-            ls.show_message_log(f'Error ocurred: {e}')
-
-    ls.get_configuration(
-        WorkspaceConfigurationParams(
-            items=[
-                ConfigurationItem(
-                    scope_uri='',
-                    section=JsonLanguageServer.CONFIGURATION_SECTION)
-            ]
-        ),
-        _config_callback
-    )
+#     except Exception as e:
+#         ls.show_message_log(f'Error ocurred: {e}')
 
 
-@json_server.thread()
-@json_server.command(JsonLanguageServer.CMD_SHOW_CONFIGURATION_THREAD)
-def show_configuration_thread(ls: JsonLanguageServer, *args):
-    """Gets exampleConfiguration from the client settings using thread pool."""
-    try:
-        config = ls.get_configuration(WorkspaceConfigurationParams(items=[
-            ConfigurationItem(
-                scope_uri='',
-                section=JsonLanguageServer.CONFIGURATION_SECTION)
-        ])).result(2)
+# @cylc_ls.command(CylcLanguageServer.CMD_SHOW_CONFIGURATION_CALLBACK)
+# def show_configuration_callback(ls: CylcLanguageServer, *args):
+#     """Gets exampleConfiguration from the client settings using callback."""
+#     def _config_callback(config):
+#         try:
+#             example_config = config[0].get('exampleConfiguration')
 
-        example_config = config[0].get('exampleConfiguration')
+#             ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
 
-        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+#         except Exception as e:
+#             ls.show_message_log(f'Error ocurred: {e}')
 
-    except Exception as e:
-        ls.show_message_log(f'Error ocurred: {e}')
+#     ls.get_configuration(
+#         WorkspaceConfigurationParams(
+#             items=[
+#                 ConfigurationItem(
+#                     scope_uri='',
+#                     section=CylcLanguageServer.CONFIGURATION_SECTION)
+#             ]
+#         ),
+#         _config_callback
+#     )
 
 
-@json_server.command(JsonLanguageServer.CMD_UNREGISTER_COMPLETIONS)
-async def unregister_completions(ls: JsonLanguageServer, *args):
-    """Unregister completions method on the client."""
-    params = UnregistrationParams(unregisterations=[
-        Unregistration(id=str(uuid.uuid4()), method=TEXT_DOCUMENT_COMPLETION)
-    ])
-    response = await ls.unregister_capability_async(params)
-    if response is None:
-        ls.show_message('Successfully unregistered completions method')
-    else:
-        ls.show_message('Error happened during completions unregistration.',
-                        MessageType.Error)
+# @cylc_ls.thread()
+# @cylc_ls.command(CylcLanguageServer.CMD_SHOW_CONFIGURATION_THREAD)
+# def show_configuration_thread(ls: CylcLanguageServer, *args):
+#     """Gets exampleConfiguration from the client settings using thread pool."""
+#     try:
+#         config = ls.get_configuration(WorkspaceConfigurationParams(items=[
+#             ConfigurationItem(
+#                 scope_uri='',
+#                 section=CylcLanguageServer.CONFIGURATION_SECTION)
+#         ])).result(2)
+
+#         example_config = config[0].get('exampleConfiguration')
+
+#         ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+
+#     except Exception as e:
+#         ls.show_message_log(f'Error ocurred: {e}')
+
+
+# @cylc_ls.command(CylcLanguageServer.CMD_UNREGISTER_COMPLETIONS)
+# async def unregister_completions(ls: CylcLanguageServer, *args):
+#     """Unregister completions method on the client."""
+#     params = UnregistrationParams(unregisterations=[
+#         Unregistration(id=str(uuid.uuid4()), method=TEXT_DOCUMENT_COMPLETION)
+#     ])
+#     response = await ls.unregister_capability_async(params)
+#     if response is None:
+#         ls.show_message('Successfully unregistered completions method')
+#     else:
+#         ls.show_message('Error happened during completions unregistration.',
+#                         MessageType.Error)
